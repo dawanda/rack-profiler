81b43c1ce04dfbee4e8056b71c7e00f923110fa1
Luca Tironi <luca.tironi@gmail.com>
2015-01-28 01:17:13 +0100

    Setup CodeClimate test report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'rack/profiler'
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rack/profiler'
